1248d8d5b8116c670324d47004dd8c77f2b08bf2
91077063 <palmdigital@PLMS-SC1-S03.mvusd.k12.ca.us>
2016-11-30 08:01:31 -0800

    alot of changes

--- section05recursion/SpiderWeb.java
+++ section05recursion/SpiderWeb.java
@@ -1,6 +1,8 @@
 package org.teachingkidsprogramming.section05recursion;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SpiderWeb
 {
@@ -8,31 +10,26 @@ public class SpiderWeb
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
-    //    Change the Tortoise to a Spider --#14
-    //    Change the pen color of the line the tortoise draws to red --#13.1
-    //    Set the color of the background window to black (HINT: use the Tortoise to get the background window) --#13.2
+    Tortoise.setPenWidth(1);
+    Tortoise.setAnimal(Animals.Spider);
+    Tortoise.setPenColor(PenColors.Whites.White);
+    Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.Black);
     double lineLength = 10.5;
     double zoom = 1.1;
-    //    Do the following 10 times --#10.1
-    lineLength = weaveOneLayer(lineLength, zoom);
-    //
-    //        Change the current zoom so it is multiplied by 1.3 --#11
-    //    Repeat --#10.2
+    for (int i = 0; i < 10; i++)
+    {
+      lineLength = weaveOneLayer(lineLength, zoom);
+      zoom = zoom * 1.3;
+    }
   }
   private static double weaveOneLayer(double lineLength, double zoom)
   {
-    //        weaveOneLayer (recipe below) --#9.1
-    //
-    //        ------------- Recipe for weaveOneLayer --#9.2
     for (int i = 0; i < 6; i++)
     {
       drawTriangle(lineLength);
-      //
       Tortoise.turn(360 / 6);
       lineLength = lineLength + zoom;
     }
-    //        ------------- End of weaveOneLayer recipe --#9.3
     return lineLength;
   }
   private static void drawTriangle(double lineLength)
@@ -40,7 +37,7 @@ public class SpiderWeb
     for (int i = 0; i < 3; i++)
     {
       Tortoise.move(lineLength);
-      Tortoise.turn(360 / 3);
+      Tortoise.turn(360 / 6);
     }
   }
 }
